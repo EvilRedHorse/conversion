a7e9dd345cf0766ca0a799a1179f0fa3a1ff0cb0
EvilRedHorse <smc@localhost.localdomain>
2019-04-23 19:40:58 -0400

    add commit.sh

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/sh
-printf "make sure you have author set, this will commit\nCommit message:"
+printf "make sure you have author set, this will commit\nCommit message: "
 read MESSAGE
 git add -A && git commit -m "$MESSAGE"
-printf "now.../ngit push"
+printf "now...\ngit push\n"
