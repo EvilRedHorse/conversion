d8b2cfabd17e015900370c2ca3a3846222379897
EvilRedHorse <evilredhorse@gmail.com>
2019-06-02 01:22:14 -0400

    added scientific notation to float for -ve exponent values

--- commit.sh
+++ commit.sh
@@ -1,5 +1,12 @@
-#! /usr/bin/sh
-printf "make sure you have author set, this will commit\nCommit message: "
+#!/usr/bin/env bash
+git config --global gpg.program gpg2
+git config user.name EvilRedHorse
+git config user.email evilredhorse@gmail.com
+git config user.signingkey 923F1CD1575F06DD
+git config commit.gpgsign true
+printf "author set to EvilRedHorse, this will commit\nCommit message: "
 read MESSAGE
 git add -A && git commit -m "$MESSAGE"
-printf "now...\ngit push\n"
+printf "now...\ngit push - Username & Password required\n\n"
+# git push update
+git push origin master
