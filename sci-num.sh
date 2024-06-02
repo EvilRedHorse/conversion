02483be613ca1a5d14ff06c779b27c8c44237a78
EvilRedHorse <39168275+EvilRedHorse@users.noreply.github.com>
2024-06-02 10:41:19 -0400

    Update sci-num.sh
    updated to include negative exponents

--- sci-num.sh
+++ sci-num.sh
@@ -1,10 +1,24 @@
 #! /usr/bin/sh
-# Attention: only works for negative exponents
-# Title: scientific notation to float
+# 
+# Title: convert if in scientific notation
 # Author: Stefan Crawford
 # Requires: bc
-# set to exit on error 
+# sudo dnf -y install bc
+# set to exit on error
 set -o errexit
-# take in scientific notation
-read SCI
-echo "scale=${#SCI}+${SCI##*e-}; ${SCI%e*}"^"${SCI##*e}" | bc
+
+sci_remove () {
+# convert price when listed in scientific notation
+NUM=${1,,};
+if [[ $NUM == *"e-"* ]]; then
+    NUM=$( echo "scale=${#NUM}+${NUM##*e-}; ${NUM%e*}"*"10^${NUM##*e}" | bc -l);
+    echo $NUM;
+elif [[ $NUM == *"e+"* ]]; then
+    NUM=$( echo "scale=0; ${NUM%e*}/1"*"10^${NUM##*+}" | bc -l);
+    echo $NUM;
+else
+    echo $NUM;
+fi
+}
+# take in scientific notation from $1 to convert from sci or return original value
+sci_remove $1;
